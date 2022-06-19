bb90f982dafe97d4dac35e23384747ab82e17a66
mercklyzer <mercklyzer@gmail.com>
2022-06-19 16:45:19 +0800

    Restore side user component because other components are still dependent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { HeroComponent } from './hero/hero.component';
 import { QuestionComponent } from './question/question.component';
 import { SubjectsComponent } from './subjects/subjects.component';
+import { SideUserComponent } from './side-user/side-user.component';
 import { AnswerComponent } from './answer/answer.component';
 import { CommentComponent } from './comment/comment.component';
 import { AnswersComponent } from './answers/answers.component';
@@ -41,6 +42,7 @@ const config: SocketIoConfig = { url: environment.apiUrl, options: {} };
     HeroComponent,
     QuestionComponent,
     SubjectsComponent,
+    SideUserComponent,
     AnswerComponent,
     CommentComponent,
     AnswersComponent,
---
+++ src/app/side-user/side-user.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SideUserComponent } from './side-user.component';
+
+describe('SideUserComponent', () => {
+  let component: SideUserComponent;
+  let fixture: ComponentFixture<SideUserComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SideUserComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SideUserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
